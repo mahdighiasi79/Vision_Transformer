89b4fd2b5aa137d801e9dc40db9d2c44ac393a35
Mahdi Ghiasi <mmghf74@gmail.com>
2025-11-12 16:57:22 +0100

    data augmentation techniques added

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import argparse
 
-import pytest
+# import pytest
 import torch
 from torch import nn
 from torchvision import datasets, transforms, models
 from torch.utils.data import DataLoader
 from torch import optim
 
-from models import ViT, CrossViT   # rename the skeleton file for your implementation / comment before testing for ResNet
+from models import ViT, CrossViT  # rename the skeleton file for your implementation / comment before testing for ResNet
 
 
 def parse_args():
@@ -19,10 +19,12 @@ def parse_args():
     parser.add_argument('--momentum', type=float, default=0.9, help='SGD momentum (default: 0.9)')
     parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
     parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, help='how many batches to wait before logging training status')
+    parser.add_argument('--log-interval', type=int, default=10,
+                        help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=False, help='For Saving the current Model')
     parser.add_argument('--dry-run', action='store_true', default=False, help='quickly check a single pass')
     return parser.parse_args()
+
 
 def train(model, trainloader, optimizer, criterion, device, epoch):
     model.train()
@@ -30,7 +32,7 @@ def train(model, trainloader, optimizer, criterion, device, epoch):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = criterion(output, target)/len(output)
+        loss = criterion(output, target) / len(output)
         loss.backward()
         optimizer.step()
         if batch_idx % args.log_interval == 0:
@@ -39,6 +41,7 @@ def train(model, trainloader, optimizer, criterion, device, epoch):
                        100. * batch_idx / len(trainloader), loss.item()))
             if args.dry_run:
                 break
+
 
 def run_test(model, device, test_loader, criterion, set="Test"):
     model.eval()
@@ -61,20 +64,28 @@ def run_test(model, device, test_loader, criterion, set="Test"):
 
 def run(args):
     # Download and load the training data
-    transform = transforms.Compose([transforms.ToTensor(),
-                                    # ImageNet mean/std values should also fit okayish for CIFAR
-									transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225) ) 
-                                    ])
+    transform = transforms.Compose([
+        # ImageNet mean/std values should also fit okayish for CIFAR
+        # transforms.RandomCrop(26),
+        # transforms.RandomHorizontalFlip(p=0.5),
+        # transforms.RandomResizedCrop(size=32, scale=(0.08, 1.0)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    ])
 
     # TODO: adjust folder
-    dataset = datasets.CIFAR10('..\\Exercise01 - ViT and Cross ViT\\train_images', download=True, train=True, transform=transform)
-    trainset, valset = torch.utils.data.random_split(dataset, [int(len(dataset)*0.9), len(dataset)-int(len(dataset)*0.9)])
+    dataset = datasets.CIFAR10('..\\Exercise01 - ViT and Cross ViT\\train_images', download=True, train=True,
+                               transform=transform)
+    trainset, valset = torch.utils.data.random_split(dataset,
+                                                     [int(len(dataset) * 0.9), len(dataset) - int(len(dataset) * 0.9)])
+
     trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
     valloader = DataLoader(valset, batch_size=64, shuffle=False)
 
     # Download and load the test data
     # TODO: adjust folder
-    testset = datasets.CIFAR10('..\\Exercise01 - ViT and Cross ViT\\test_images', download=True, train=False, transform=transform)
+    testset = datasets.CIFAR10('..\\Exercise01 - ViT and Cross ViT\\test_images', download=True, train=False,
+                               transform=transform)
     testloader = DataLoader(testset, batch_size=64, shuffle=True)
 
     # Build a feed-forward network
@@ -82,19 +93,22 @@ def run(args):
     if args.model == "r18":
         model = models.resnet18(pretrained=False)
     elif args.model == "vit":
-        model = ViT(image_size = 32, patch_size = 8, num_classes = 10, dim = 64,
-                    depth = 2, heads = 8, mlp_dim = 128, dropout = 0.1,
-                    emb_dropout = 0.1) 
+        model = ViT(image_size=32, patch_size=8, num_classes=10, dim=64,
+                    depth=2, heads=8, mlp_dim=128, dropout=0.1,
+                    emb_dropout=0.1)
     elif args.model == "cvit":
-        model = CrossViT(image_size = 32, num_classes = 10, sm_dim = 64, 
-                         lg_dim = 128, sm_patch_size = 8, sm_enc_depth = 2,
-                         sm_enc_heads = 8, sm_enc_mlp_dim = 128, 
-                         sm_enc_dim_head = 64, lg_patch_size = 16, 
-                         lg_enc_depth = 2, lg_enc_heads = 8, 
-                         lg_enc_mlp_dim = 128, lg_enc_dim_head = 64,
-                         cross_attn_depth = 2, cross_attn_heads = 8,
-                         cross_attn_dim_head = 64, depth = 3, dropout = 0.1,
-                         emb_dropout = 0.1)
+        model = CrossViT(image_size=32, num_classes=10, sm_dim=64,
+                         lg_dim=128, sm_patch_size=8, sm_enc_depth=2,
+                         sm_enc_heads=8, sm_enc_mlp_dim=128,
+                         sm_enc_dim_head=64, lg_patch_size=16,
+                         lg_enc_depth=2, lg_enc_heads=8,
+                         lg_enc_mlp_dim=128, lg_enc_dim_head=64,
+                         cross_attn_depth=2, cross_attn_heads=8,
+                         cross_attn_dim_head=64, depth=3, dropout=0.1,
+                         emb_dropout=0.1)
+    else:
+        print("not a valid model entered in the arguments. Please select between: {r18, vit, cvit}")
+        return
 
     # Define the loss
     criterion = nn.CrossEntropyLoss(reduction="sum")
@@ -110,6 +124,7 @@ def run(args):
         run_test(model, device, valloader, criterion, set="Validation")
 
     run_test(model, device, testloader, criterion)
+
 
 if __name__ == '__main__':
     args = parse_args()
